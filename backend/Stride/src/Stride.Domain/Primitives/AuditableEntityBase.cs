6fa9712f97869ab95715c63e8e9f059bea55a124
Summist <dima.kovalov.05@mail.ru>
2026-07-11 22:20:51 +0300

    Создать базовые классы для доменных сущностей

---
+++ backend/Stride/src/Stride.Domain/Primitives/AuditableEntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Stride.Domain.Primitives;
+
+public abstract class AuditableEntityBase : EntityBase
+{
+    public DateTime CreatedAtUtc { get; set; }
+
+    public DateTime? UpdatedAtUtc { get; set; }
+}
---
+++ backend/Stride/src/Stride.Domain/Primitives/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Stride.Domain.Primitives;
+
+public abstract class EntityBase
+{
+    public Guid Id { get; set; }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj is null || obj.GetType() != GetType()) return false;
+
+        return Id == ((EntityBase)obj).Id;
+    }
+}
